7e0a40012900e90e927d438d3f0371132ddd3ec6
Saket.Webonise Pandhare <saket.webonisepandhare@SaketWebonises-iMac.local>
2017-12-18 15:13:40 +0530

    Changes done for removing pods

--- GSDemo/AppDelegate.swift
+++ GSDemo/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var backgroundSessionCompletionHandler: (() -> Void)?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -39,6 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+        backgroundSessionCompletionHandler = completionHandler
     }
 
 
--- GSDemo/ViewController.swift
+++ GSDemo/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         getBaseUrl()
+        exampleAPI1()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +28,74 @@ class ViewController: UIViewController {
     func getBaseUrl() {
         let baseURL = Bundle.main.object(forInfoDictionaryKey: "BASEURL")
         labelPointingUrl.text = baseURL as! String?
+    }
+    
+    func exampleAPI() {
+        let URL: NSURL = NSURL(string: "https://itunes.apple.com/lookup?id=909253")!
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: URL as URL)
+        request.httpMethod = "GET"
+        // Convert POST string parameters to data using UTF8 Encoding
+//        let postParams = "api_key=APIKEY&email=example@example.com&password=password"
+//        let postData = postParams.data(using: .utf8)
+        //request.httpBody = "requestPara" /// This fucntion is used to send request parameters
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest) {data,response,error in
+            
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
+                    print("ASynchronous\(jsonResult)")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        dataTask.resume()
+    }
+    
+    /// This is APi call using NSURL session with URL Session configuration
+    /// By using nsurl session we can keep progress of download of any file fron server for e.g music file 
+    /// we can also pause,resume and cancel download
+    
+    func exampleAPI1() {
+        let URL = NSURL(string: "https://itunes.apple.com/lookup?id=909253")!
+        let requestURL: NSMutableURLRequest = NSMutableURLRequest(url: URL as URL)
+        requestURL.httpMethod = "GET"
+        
+        
+        // Convert POST string parameters to data using UTF8 Encoding
+        //        let postParams = "api_key=APIKEY&email=example@example.com&password=password"
+        //        let postData = postParams.data(using: .utf8)
+        //request.httpBody = "requestPara" /// This fucntion is used to send request parameters
+        
+        /// session configuratin is used to store data regarding request info like header value,
+        /// cellular access , timeout interval, caching policy 
+        /// we can make a common class an use it through out the app
+        let sessionConfiguration = URLSessionConfiguration.default
+        
+        ///Returns a session configuration that uses no persistent storage for caches, cookies, or credentials.
+       // let sessionConfiguration = URLSessionConfiguration.ephemeral
+        
+        sessionConfiguration.allowsCellularAccess = false
+        sessionConfiguration.timeoutIntervalForRequest = 30
+        
+        
+        let session =  URLSession(configuration: sessionConfiguration)
+        
+        
+        /// Below session is used to download any fie when app is in background mode 
+        ///You must not create more than one session for a background configuration, because the system uses the configuration‘s identifier to associate tasks with the session.
+        /// If a background task completes when the app isn’t running, the app will be relaunched in the background. You’ll need to handle this event from your app delegate.
+        
+     //   let session = URLSession(configuration: URLSessionConfiguration.background(withIdentifier: "bgConfiguration"))
+        
+        
+        session.dataTask(with: requestURL as URLRequest) {(data, response, error) -> Void in
+            if let data = data {
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options: [])
+                print("Json data has \(String(describing: jsonData))")
+            }
+        }.resume()
     }
 }
 
